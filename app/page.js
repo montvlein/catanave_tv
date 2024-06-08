8d37940a31912e6f2095ddb285f743e73cf09de6
montlein <montvlein@gmail.com>
2024-06-08 20:03:31 -0300

    initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <Hero/>
+    </main>
+  );
+}
+
+function Hero() {
+  return(
+    <section className="bg-green-700 h-dvh">
+      <span>Catannabis Tv</span>
+    </section>
+  )
+}
