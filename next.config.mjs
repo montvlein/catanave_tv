5d7fd588f619e48fefbd0b3a894b39cc3653e87d
montvlein <montvlein@gmail.com>
2024-10-09 00:34:48 -0300

    git revert

--- next.config.mjs
+++ next.config.mjs
@@ -12,17 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      }
-    }
-    return config
-  },
 };
 
 export default MillionLint.next({
