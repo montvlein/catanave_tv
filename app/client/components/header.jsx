7f4d624fa270a05f8cb2a42582ee35f56c3a7bed
montvlein <montvlein@gmail.com>
2024-10-28 00:44:17 -0300

    se agrego boton para minimisar el menu

--- app/client/components/header.jsx
+++ app/client/components/header.jsx
@@ -5,7 +5,7 @@ function Header({title}) {
     return (
         <header className="bg-cyan-900 p-4 flex flex-row-reverse gap-4 justify-end items-center">
             <h1 className="text-semibold text-xl text-font-playwrite-gb-semijoined" >{title}</h1>
-            <nav className="flex items-center">
+            <div className="flex items-center">
                 <Link href={"/"} className="flex flex-row justify-center items-center gap-4 text-font-playwrite-gb-semijoined">
                 <Image
                     src={"/logo.png"}
@@ -13,7 +13,7 @@ function Header({title}) {
                     height={40}
                 ></Image>
                 </Link>
-            </nav>
+            </div>
         </header>
     )
 }
