13618c535528fd59f315857f6c7210ea3163cd70
montvlein <montvlein@gmail.com>
2024-10-27 21:36:32 -0300

    se unifico header en un componente

---
+++ app/client/components/header.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header({title}) {
+    return (
+        <header className="bg-cyan-900 p-4 flex flex-row-reverse gap-4 justify-end items-center">
+            <h1 className="text-semibold text-xl text-font-playwrite-gb-semijoined" >{title}</h1>
+            <nav className="flex items-center">
+                <Link href={"/"} className="flex flex-row justify-center items-center gap-4 text-font-playwrite-gb-semijoined">
+                <Image
+                    src={"/logo.png"}
+                    width={40}
+                    height={40}
+                ></Image>
+                </Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
