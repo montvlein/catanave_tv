b84af2c7a44928d5f4af5ad2396f3b5b6ca48b00
montvlein <montvlein@gmail.com>
2024-12-01 15:58:34 -0300

    CHANGE text color

--- app/(page)/proyectos/page.jsx
+++ app/(page)/proyectos/page.jsx
@@ -56,7 +56,7 @@ function StoryboardCard({storyboard}) {
                 <Link href={`/proyectos/${storyboard.slug.current}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Todos los capitulos
                 </Link>
-                <Link href={`/proyectos/${storyboard.slug.current}/${lastChapter.slug.current}`} className="inline-flex items-center gap-2 px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <Link href={`/proyectos/${storyboard.slug.current}/${lastChapter.slug.current}`} className="inline-flex items-center gap-2 px-3 py-2 text-sm font-medium text-center text-white bg-lime-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-blue-800">
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
