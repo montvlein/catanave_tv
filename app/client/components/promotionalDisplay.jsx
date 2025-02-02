5c2be7d2369219904d04bb0434662fdff0e0cd3f
montvlein <montvlein@gmail.com>
2025-02-01 23:13:41 -0300

    se agrego opciones al reproductor. Se agrego sonido al reproducir

--- app/client/components/promotionalDisplay.jsx
+++ app/client/components/promotionalDisplay.jsx
@@ -1,16 +1,43 @@
 'use client'
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function PromotionalDisplay() {
+    const videoRef = useRef(null);
+    const [visible, setVisible] = useState(true)
+
     const [isPromotionDayComingUp, setIsPromotionDayComingUp] = useState(true)
     const handleEnd = () => {setIsPromotionDayComingUp(false)}
     if (!isPromotionDayComingUp) return <></>
 
+    useEffect(() => {
+        if (videoRef.current) {
+          const playPromise = videoRef.current.play();
+          if (playPromise !== undefined) {
+            playPromise
+              .then(() => {
+                setTimeout(() => {
+                  videoRef.current.muted = false;
+                }, 500);
+              })
+              .catch((error) => console.log("Autoplay bloqueado:", error));
+          }
+        }
+    }, []);
+
     return (
-        <div className="absolute z-[60] w-full h-full flex items-center bg-black/90 bg-logo bg-repeat">
+        <div className={`absolute z-[60] w-full h-full flex items-center bg-black/90 bg-logo bg-repeat ${visible? "" : "hidden"}`}>
             <div className="relative">
-                <div className="z-[61] absolute top-5 right-5 bg-white/30 text-rose-800 border-2 border-red-900 px-2 rounded-full font-bold cursor-pointer hover:bg-black">X</div>
-                <video autoPlay onEnded={handleEnd}>
+                <div
+                    className="z-[61] absolute top-5 right-5 bg-white/30 text-rose-800 border-2 border-red-900 px-2 rounded-full font-bold cursor-pointer hover:bg-black"
+                    onClick={()=>{
+                        setVisible(false)
+                        if (videoRef.current) {
+                            videoRef.current.pause();
+                            videoRef.current.currentTime = 0;
+                        }
+                    }}
+                >X</div>
+                <video ref={videoRef} autoPlay muted playsInline controls onEnded={handleEnd} controlsList="nodownload">
                     <source src={"/videos/promo.webm"} type="video/webm" />
                     <source src={"/videos/promo.mp4"} type="video/mp4" />
                     Your browser does not support the video tag.
